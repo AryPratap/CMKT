e80e431a18799501a3b1ffe3caefeab682c77940
Ary Pratap Singh <arypratapsingh1055@gmail.com>
2023-05-15 14:57:02 +0530

    Updated demo_LID.py

--- demo_LID.py
+++ demo_LID.py
@@ -3,22 +3,23 @@ from cmtt.tasks import HinglishToolKit
 mytoolkit = HinglishToolKit()
 
 
-lid = mytoolkit.BiLSTM_HINENG_LID()
+BiLSTM = mytoolkit.BiLSTM_HINENG_LID()
 sentence = 'tu kesa hai mere bhai, kyuki I am fine. Empowerment toh people choddega nhi, right?'
-lst = lid.getLangTags(sentence)
+
+lst = BiLSTM.getLangTags(sentence)
 print(lst)
-unks = lid.getUnks(sentence)
+unks = BiLSTM.getUnks(sentence)
 print(unks)
 print()
 
 sentence = 'kya haal hai bhai! Let me get activated now aur fir khelthe hai?'
-lst = lid.getLangTags(sentence)
+lst = BiLSTM.getLangTags(sentence)
 print(lst)
-unks = lid.getUnks(sentence)
+unks = BiLSTM.getUnks(sentence)
 print(unks)
 
 sentence = "Hey, how are you bhai? It has been kaafi lamba time since we last met."
-lst = lid.getLangTags(sentence)
+lst = BiLSTM.getLangTags(sentence)
 print(lst)
-unks = lid.getUnks(sentence)
-print(unks)
+unks = BiLSTM.getUnks(sentence)
+print(unks) 
