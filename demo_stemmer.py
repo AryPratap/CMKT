ffad8c015bebe12bf4580059fefbd84090cd831a
Ary Pratap Singh <arypratapsingh1055@gmail.com>
2023-05-15 23:26:09 +0530

    Updated stemmer demo files

--- demo_stemmer.py
+++ demo_stemmer.py
@@ -20,9 +20,3 @@ print(hindi_stemmed)
 with open(r"test_hindi_stemmed.txt", 'w', encoding = "utf-8") as f:
   f.write(hindi_stemmed + "\n")
 
-code_mixed_text = 'tu kesa hai mere bhai, kyuki I am fine. Empowerment toh people chaddange nhi, right?'
-hien_stemmed = hien_stemmer(code_mixed_text)
-hiennn = ' '.join(hien_stemmed)
-print(hien_stemmed)
-with open(r"test_hien_stemmed.txt", 'w', encoding = "utf-8") as f:
-  f.write(hiennn + "\n")
