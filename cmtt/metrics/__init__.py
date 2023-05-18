acca591f1e106336fb55b984149c1c2a03911a7b
Ary Pratap Singh <arypratapsingh1055@gmail.com>
2023-05-18 22:14:43 +0530

    Added the metrics module

--- cmtt/metrics/__init__.py
+++ cmtt/metrics/__init__.py
@@ -1,44 +1,141 @@
-# Finding CMI using only tags
-# N - total number of tokens exclusing independent tags
-# max_cnt - count of words occuring in maximum language
-# P - number of switches between langauges
-# CMI of a utternace Cu = ((N-max_cnt+P)*100)/(2*N)
-
-tags = {'0':'EN', '1':'HI', '2':'UN'}
-
-def utteranceCMI(sentence):
-	count = [0,0,0]
-	listOfUtterance = sentence.split(" ")
-	print(listOfUtterance)  
-	count[0] = listOfUtterance.count("EN")
-	count[1] = listOfUtterance.count("HI")
-	count[2] = listOfUtterance.count("UN")
-	print(count)
-	max_cnt = max(count)
-	N = count[0]+count[1]
-	if N==0:
-		return 0
-	P = 0
-	prev = listOfUtterance[0]
-	for i in range(1, len(listOfUtterance)):
-		if listOfUtterance[i]!='UN':
-			if prev!=listOfUtterance[i]:
-				P+=1
-				prev = listOfUtterance[i]
-	Cu = (100.0*(N-max_cnt+P))/(2.0*N)
-	return Cu
-
+from cmtt.tasks import HinglishToolKit
+import numpy as np
 
 def cmi(sentence):
-  count = [0,0,0]
-  listOfUtterance = sentence.split(" ")
-  count[0] = listOfUtterance.count("EN")
-  count[1] = listOfUtterance.count("HI")
-  count[2] = listOfUtterance.count("UN")
+	"""
+    It calculates Code-Mixing-Index
+    Args: 
+    sentence: String containing the input sentence. 
+    output:
+    CMI: Code mixing Index of given sentence
+    """
+	mytoolkit = HinglishToolKit()
+	lid = mytoolkit.XLM_HIEN_LID()
 
-  max_cnt = max(count)
+	langTags = lid.getlangIds(sentence)
+	ner = mytoolkit.XLM_HIEN_NER()
+	nertags = ner.getNERTags(sentence)
 
-  N = count[0]+count[1]+count[2]
+	for i in range(len(langTags)):
+		if nertags[i][1] != 'O':
+			langTags[i] = '8'
+				
+	lang1_words = 0
+	for i in langTags:
+		if i == '0':
+			lang1_words = lang1_words + 1
+			
+	lang2_words = 0
+	for i in langTags:
+		if i == '1':
+			lang2_words = lang2_words + 1
+			
+	max_count = max(lang1_words,lang2_words)
 
-  cmi = 100.0*(1.0 - (max_cnt)/(N - count[2]))
-  return cmi
+	# total tokens 
+	n = len(langTags)
+	# language independent tokens
+	u = len(langTags) - lang1_words - lang2_words
+
+	if n>u:
+		return 100*(1 - (max_count/(n-u)))
+	return 0
+
+def M_Index(sentence):
+	mytoolkit = HinglishToolKit()
+	lid = mytoolkit.XLM_HIEN_LID()
+
+	langTags = lid.getlangIds(sentence)
+	ner = mytoolkit.XLM_HIEN_NER()
+	nertags = ner.getNERTags(sentence)
+
+	for i in range(len(langTags)):
+		if nertags[i][1] != 'O':
+			langTags[i] = '8'
+				
+	lang1_words = 0
+	for i in langTags:
+		if i == '0':
+			lang1_words = lang1_words + 1
+			
+	lang2_words = 0
+	for i in langTags:
+		if i == '1':
+			lang2_words = lang2_words + 1
+	
+	# total number of words 
+	n = len(langTags)
+	# total languages 
+	k = 3
+
+	# language independent words
+	lang3_words = n - lang1_words - lang2_words
+	if lang3_words == 0:
+		k = 2
+	
+	sigma_pj = (lang1_words/n)**2 + (lang2_words/n)**2 + (lang3_words/n)**2 
+
+	return (1- sigma_pj)/((k-1)*sigma_pj)
+
+def I_index(sentence):
+	mytoolkit = HinglishToolKit()
+	lid = mytoolkit.XLM_HIEN_LID()
+
+	langTags = lid.getlangIds(sentence)
+	ner = mytoolkit.XLM_HIEN_NER()
+	nertags = ner.getNERTags(sentence)
+
+	for i in range(len(langTags)):
+		if nertags[i][1] != 'O':
+			langTags[i] = '8'
+
+	S = 0
+
+	for i in range(len(langTags)-1):
+		if langTags[i]!=langTags[i+1]:
+			S = S+1
+		else:
+			s = S + 0
+	#return S/(len(langTags)-1)
+	return langTags
+
+def burstiness(sentence):
+	mytoolkit = HinglishToolKit()
+	lid = mytoolkit.XLM_HIEN_LID()
+
+	langTags = lid.getlangIds(sentence)
+	ner = mytoolkit.XLM_HIEN_NER()
+	nertags = ner.getNERTags(sentence)
+
+	for i in range(len(langTags)):
+		if nertags[i][1] != 'O':
+			langTags[i] = '8'   
+
+    # Calculate the language spans
+	spans = []
+	current_span = 1
+	for i in range(1, len(langTags)):
+		if langTags[i] == langTags[i-1]:
+			current_span += 1
+		else:
+			spans.append(current_span)
+			current_span = 1
+	spans.append(current_span)
+
+    # Calculate the mean and standard deviation of language spans
+	mr = np.mean(spans)
+	sigma_r = np.std(spans)
+
+    # Calculate burstiness
+	burstiness = (sigma_r - mr) / (sigma_r + mr)
+	return burstiness
+	
+		
+
+
+
+	
+	
+
+					
+		
--- demo_test.py
+++ demo_test.py
@@ -1,15 +1,34 @@
 from cmtt.tasks import HinglishToolKit
 from cmtt.preprocessing import hien_stemmer
+from cmtt.metrics import M_Index
+from cmtt.metrics import I_index, burstiness
 
 mytoolkit = HinglishToolKit()
 
-
+'''
 code_mixed_text = 'tu kesa hai mere bhai, kyuki I am fine. Empowerment toh people chaddange nhi, right?'
 hien_stemmed = hien_stemmer(code_mixed_text)
 hiennn = ' '.join(hien_stemmed)
 print(hien_stemmed)
 with open(r"test_hien_stemmed.txt", 'w', encoding = "utf-8") as f:
   f.write(hiennn + "\n")
+'''
+
+sentence = "Deepak ji, channel ko kitna fund diya hai congress ne? 2006 me ameithi rape case kyu nahi discuss kiya kabhi?"
+sentence = "RAHUL jab dieting par hota hai toh green tea peeta hai."
+#sentence = "Deepak ji, channel ko kitna fund diya hai congress ne? 2006 me ameithi rape case kyu nahi discuss kiya kabhi?"
+#sentence = "4 din me 2 accidents, kuch to jhol hai, shayad politics ho rahi hai.."
+#sentence = "The ultimate twist Dulhan dandanate huye brings Baraat .... Dulha"
+#sentence = "laufed ... first u hav to correct ur english baad me sochna use !!!"
+#lid = mytoolkit.XLM_HIEN_LID()
+#print(lid.getLangTags(sentence))
+
+#ner = mytoolkit.XLM_HIEN_NER()
+#print(ner.getNERTags(sentence))
+print(burstiness(sentence))
+
+
+
 
 #ner = mytoolkit.XLM_HIEN_NER()
 
